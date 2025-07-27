45fa7f7c647e304048c1f89c170820e13b6f53af
Alexander <alexanderfurholt@gmail.com>
2025-07-27 16:33:31 +0200

    Adjustments

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// Noe i denne applikasjonen her
+function addNumbers(num1, num2) {
+    if (num1 > num2) {
+        return "problem"
+    }
+    return num1 + num2
+}
+
+
+const res1 = addNumbers(1, 5);
+const res2 = addNumbers(1, 10);
+
+console.log(res1);
+console.log(res2);
+
+const res3 = addNumbers(res2, res1)
+
+console.log(res3);
